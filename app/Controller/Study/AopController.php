5a9355425ec50787d4bb889d498b46af93a0eedb
清流 <ruoge3s@qq.com>
2020-02-09 18:36:45 +0800

    学习aop面向切片编程

---
+++ app/Controller/Study/AopController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Study;
+
+use App\Controller\AbstractController;
+
+class AopController extends AbstractController
+{
+    public function moduleName(): string
+    {
+        return 'study';
+    }
+
+
+}
